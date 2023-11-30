6f154cebf2328290178de0dcc62aee0e377ea1f7
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2023-11-30 16:29:40 +0100

    set up the cli base

---
+++ svync.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	cli "github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:            "svync",
+		Usage:           "A tool to standardize VCF files from structural variant callers",
+		HideHelpCommand: true,
+		Version:         "0.1.0dev",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "output",
+				Aliases:  []string{"o"},
+				Usage:    "The location to the output VCF file, defaults to stdout",
+				Category: "Optional",
+			},
+			&cli.StringFlag{
+				Name:     "config",
+				Aliases:  []string{"c"},
+				Usage:    "Configuration file (YAML) to use for the parsing of INFO and FORMAT fields",
+				Required: true,
+				Category: "Required",
+			},
+			&cli.StringFlag{
+				Name:     "input",
+				Aliases:  []string{"i"},
+				Usage:    "The input VCF file to standardize",
+				Required: true,
+				Category: "Required",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			logger := log.New(os.Stderr, "", 0)
+			logger.Print("Hello World!")
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
