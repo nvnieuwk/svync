5332a7b6c893b4549e27d81fa9420c33ccb60088
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2023-12-05 17:00:31 +0100

    add vcf structs + header conversion

--- svync_api/read.go
+++ svync_api/read.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/biogo/hts/bgzf"
@@ -21,16 +22,17 @@ func Read(Cctx *cli.Context) {
 	if err != nil {
 		logger.Fatal(err)
 	}
-	lines := []string{}
+
+	vcf := VCF{}
 	if strings.HasSuffix(file, ".gz") {
-		lines = readBgzip(openFile)
+		vcf = readBgzip(openFile)
 	} else {
-		lines = readPlain(openFile)
+		vcf = readPlain(openFile)
 	}
-	logger.Println(strings.Join(lines, "\n"))
+	logger.Println(vcf)
 }
 
-func readBgzip(input *os.File) []string {
+func readBgzip(input *os.File) VCF {
 	logger := log.New(os.Stderr, "", 0)
 
 	bgReader, err := bgzf.NewReader(input, 1)
@@ -39,8 +41,7 @@ func readBgzip(input *os.File) []string {
 	}
 	defer bgReader.Close()
 
-	var data []string
-
+	vcf := VCF{}
 	var n int
 	for {
 		n++
@@ -52,10 +53,10 @@ func readBgzip(input *os.File) []string {
 			logger.Fatal(string(b[:]))
 		}
 
-		data = append(data, string(bytes.TrimSpace(b[:])))
+		vcf.Parse(string(bytes.TrimSpace(b[:])))
 	}
 
-	return data
+	return vcf
 
 }
 
@@ -80,18 +81,120 @@ func readLine(r *bgzf.Reader) ([]byte, bgzf.Chunk, error) {
 	return data, chunk, err
 }
 
-func readPlain(input *os.File) []string {
+func readPlain(input *os.File) VCF {
 	logger := log.New(os.Stderr, "", 0)
 
-	var data []string
+	vcf := VCF{}
 	scanner := bufio.NewScanner(input)
 	for scanner.Scan() {
-		data = append(data, scanner.Text())
+		vcf.Parse(scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
 		logger.Fatal(err)
 	}
+
+	return vcf
+}
+
+func (vcf *VCF) Parse(line string) {
+	// logger := log.New(os.Stderr, "", 0)
+
+	if strings.HasPrefix(line, "##") {
+		vcf.Header.Parse(line)
+	}
+}
+
+func (header *Header) Parse(line string) {
+	r := regexp.MustCompile(`^##(?P<headerType>[^=]*)=<(?P<content>.*)>$`)
+	matches := r.FindStringSubmatch(line)
+
+	header.Other = []string{}
+	if len(matches) == 0 {
+		header.Other = append(header.Other, line)
+		return
+	}
+
+	headerType := matches[1]
+	content := matches[2]
+	contentMap := convertLineToMap(content)
+
+	switch headerType {
+	case "INFO":
+		if header.Info == nil {
+			header.Info = map[string]HeaderLineIdNumberTypeDescription{}
+		}
+		header.Info[contentMap["id"]] = HeaderLineIdNumberTypeDescription{
+			Id:          contentMap["id"],
+			Number:      contentMap["number"],
+			Type:        contentMap["type"],
+			Description: contentMap["description"],
+		}
+	case "FORMAT":
+		if header.Format == nil {
+			header.Format = map[string]HeaderLineIdNumberTypeDescription{}
+		}
+		header.Format[contentMap["id"]] = HeaderLineIdNumberTypeDescription{
+			Id:          contentMap["id"],
+			Number:      contentMap["number"],
+			Type:        contentMap["type"],
+			Description: contentMap["description"],
+		}
+	case "ALT":
+		if header.Alt == nil {
+			header.Alt = map[string]HeaderLineIdDescription{}
+		}
+		header.Alt[contentMap["id"]] = HeaderLineIdDescription{
+			Id:          contentMap["id"],
+			Description: contentMap["description"],
+		}
+	case "FILTER":
+		if header.Filter == nil {
+			header.Filter = map[string]HeaderLineIdDescription{}
+		}
+		header.Filter[contentMap["id"]] = HeaderLineIdDescription{
+			Id:          contentMap["id"],
+			Description: contentMap["description"],
+		}
+	case "contig":
+		if header.Contig == nil {
+			header.Contig = map[string]HeaderLineIdLength{}
+		}
+		header.Contig[contentMap["id"]] = HeaderLineIdLength{
+			Id:     contentMap["id"],
+			Length: 0,
+		}
+	}
+
+}
+
+// convertLineToMap converts the header line contents to a map suitable to transform to a struct
+func convertLineToMap(line string) map[string]string {
+	data := map[string]string{}
+	word := ""
+	key := ""
+	quote := ""
+	for _, letter := range strings.Split(line, "") {
+		if letter == "=" {
+			key = strings.ToLower(word)
+			word = ""
+			continue
+		} else if letter == "," && quote == "" {
+			data[key] = word
+			key = ""
+			word = ""
+			continue
+		}
+
+		word += letter
+
+		if letter == quote {
+			quote = ""
+		} else if letter == "\"" || letter == "'" {
+			quote = letter
+		}
+	}
+	data[key] = word
 
 	return data
 }
--- svync_api/structs.go
+++ svync_api/structs.go
@@ -1,1 +1,54 @@
 package svync_api
+
+type VCF struct {
+	Header   Header
+	Variants map[string]VariantMates
+}
+
+type Header struct {
+	Info   map[string]HeaderLineIdNumberTypeDescription
+	Format map[string]HeaderLineIdNumberTypeDescription
+	Alt    map[string]HeaderLineIdDescription
+	Filter map[string]HeaderLineIdDescription
+	Contig map[string]HeaderLineIdLength
+	Other  []string
+}
+
+type HeaderLineIdDescription struct {
+	Id          string
+	Description string
+}
+
+type HeaderLineIdNumberTypeDescription struct {
+	Id          string
+	Number      string
+	Type        string
+	Description string
+}
+
+type HeaderLineIdLength struct {
+	Id     string
+	Length int64
+}
+
+type VariantMates struct {
+	Mate1 Variant
+	Mate2 Variant
+}
+
+type Variant struct {
+	Chromosome string
+	Start      int64
+	End        int64
+	Ref        string
+	Alt        string
+	Qual       string
+	Filter     string
+	Info       map[string]string
+	Format     map[string]VariantFormat
+}
+
+type VariantFormat struct {
+	Sample string
+	Format map[string]string
+}
