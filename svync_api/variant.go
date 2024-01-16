7b66e81ca5526bf73e559ced574512a0c29c83ed
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2024-01-16 14:39:08 +0100

    remove to breakend code

--- svync_api/variant.go
+++ svync_api/variant.go
@@ -6,7 +6,6 @@ import (
 	"math"
 	"os"
 	"regexp"
-	"slices"
 	"strconv"
 	"strings"
 )
@@ -101,74 +100,4 @@ func getInsLen(alt string, strand string, bracket string) int {
 		return len(alt[strings.LastIndex(alt, bracket):])
 	}
 	return len(alt[:strings.LastIndex(alt, bracket)])
-}
-
-// Convert a breakpoint to variant to its breakend pairs
-//
-// Determining the ALT fields is impossible for breakpoint -> breakend conversion
-func (variant *Variant) toBreakEnd() (*Variant, *Variant) {
-	logger := log.New(os.Stderr, "", 0)
-
-	id1 := variant.Id + "_01"
-	id2 := variant.Id + "_02"
-
-	end, err := strconv.ParseInt(variant.Info["END"][0], 0, 64)
-	if err != nil {
-		logger.Fatal(err)
-	}
-
-	chr := variant.Chromosome
-	chr2 := ""
-	chrom2, ok := variant.Info["CHR2"]
-	if !ok {
-		chr2 = variant.Chromosome
-	} else {
-		chr2 = chrom2[0]
-	}
-
-	info1 := map[string][]string{}
-	info2 := map[string][]string{}
-	for name, info := range variant.Info {
-		if slices.Contains([]string{"CHR2", "END", "SVLEN"}, name) {
-			continue
-		}
-		info1[name] = info
-		info2[name] = info
-	}
-
-	info1["SVTYPE"] = []string{"BND"}
-	info2["SVTYPE"] = []string{"BND"}
-	info1["MATEID"] = []string{id2}
-	info2["MATEID"] = []string{id1}
-
-	alt1 := "."
-	alt2 := "."
-
-	breakend1 := &Variant{
-		Chromosome: chr,
-		Pos:        variant.Pos,
-		Id:         id1,
-		Alt:        alt1,
-		Ref:        variant.Ref,
-		Qual:       variant.Qual,
-		Filter:     variant.Filter,
-		Header:     variant.Header,
-		Info:       info1,
-		Format:     variant.Format,
-	}
-
-	breakend2 := &Variant{
-		Chromosome: chr2,
-		Pos:        end,
-		Id:         id2,
-		Alt:        alt2,
-		Ref:        "N", // TODO find a good way to determine the breakend2 REF
-		Qual:       variant.Qual,
-		Filter:     variant.Filter,
-		Header:     variant.Header,
-		Info:       info2,
-		Format:     variant.Format,
-	}
-
-	return breakend1, breakend2
 }
